ecbe175aded1d32a8fdde1ce787ab90639ff2319
Alexandr <95429700+AlxndrVolk@users.noreply.github.com>
2024-05-07 11:36:09 +0200

    Add files via upload

---
+++ services/regular.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Путь к директории
+ */
+$dir = '/hawking';
+
+/**
+ * Список файлов в дериктории
+ */
+$files = scandir($dir);
+
+/**
+ * Регулярное выражение для фильтрации
+ * с соответствием с заданием
+ * (только цифры и латинские буквы)
+ */
+$regular = '/^[a-zA-Z0-9]+\.bros$/';
+
+/**
+ * Фильтруем список
+ */
+$filteredFiles = array_filter($files, function($file) use ($regular) {
+    return preg_match($regular, $file);
+});
+
+/**
+ * Вывод на экран
+ */
+foreach ($filteredFiles as $file) {
+    echo $file . PHP_EOL;
+}
